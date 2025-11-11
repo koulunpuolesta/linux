9c3e8aa7d01722e7df99392f49f120bb446e0e1e
koulunpuolesta <nuoralamarianne2@gmail.com>
2025-11-11 22:00:57 +0200

    Add files via upload

---
+++ Linux-virtuaalikone.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<style>
+
+h1{
+font-size: 60px;}
+p{
+font-size: 50px;}
+
+strong {
+font-size:40px;}
+
+        body {
+        text-align: center;
+        background: linear-gradient(to left, #6495ED, white );
+        font-size: 24px;
+        margin: 0;
+        padding: 0;
+}
+        header {
+        background-color: #2665adff;
+        color: blue;
+}</style>
+
+
+<html>
+<head>
+
+<title>Tehtava2</title>
+</head>
+
+<body>
+
+<h1>Terve</h1>
+<p> Kello on kaksi tuntia jaljessa</p>
+<p>Kellonaika SQL-serverilta: <strong><?php echo $time; ?></strong></p>
+
+<img src="Kissa.jpg" alt="Oma kuva" widht="400" height="400">
+<p>Kuva mun kissasta</p>
+</body>
+</html>
