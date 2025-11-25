61d7a991d92b6d4b85bcb47665d765090fac9fb1
koulunpuolesta <nuoralamarianne2@gmail.com>
2025-11-25 23:22:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.markdown("<meta http-equiv='refresh' content='900'>", unsafe_allow_html=True)
 
 
-conn = mysql.connector.connect(host='localhost', user='paakaytt', password='LempPass123!', database='weather_db')
+conn = mysql.connector.connect(host='localhost', user='---', password='---', database='weather_db')
 df = pd.read_sql('SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 50',conn)
 conn.close()
 
